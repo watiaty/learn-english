2fe7321dacb6fa0c8589cd9d0133cedacaf0f4e6
waitaty <yauheni.haikou@gmail.com>
2023-11-15 08:08:58 +0300

    First commit

---
+++ src/main/java/by/waitaty/learnlang/learnlang/LearnlangApplication.java
@@ -0,0 +1,13 @@
+package by.waitaty.learnlang.learnlang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnlangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnlangApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/waitaty/learnlang/learnlang/LearnlangApplicationTests.java
@@ -0,0 +1,13 @@
+package by.waitaty.learnlang.learnlang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnlangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
